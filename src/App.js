248f138178210f9a21ba845e01faeac9ca95cb56
Reza Seidmoradi <Reza.Seidmoradi@outlook.com>
2021-05-24 11:36:33 +0430

    remove UserPage route

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import React from 'react';
-import './styles/main.css';
+import {HashRouter, Route, Switch} from "react-router-dom";
+import {Provider} from "react-redux";
+import configureStore, {history} from "./configureStore";
+import NavBar from "./components/NavBar";
+import HomePage from "./containers/HomePage";
+import LoginPage from "./containers/LoginPage";
+import {ConnectedRouter} from 'connected-react-router';
+import styles from './styles/main.css';
 
 function App() {
+    const store = configureStore({});
     return (
-        <div className="center">
-            <p>Hello World</p>
-            <p>We are using node version {process.versions.node}</p>
-            <p>electron version {process.versions.electron}</p>
-            <p>chrome version {process.versions.chrome}</p>
-        </div>
+        <Provider store={store}>
+            <ConnectedRouter history={history}>
+                <NavBar/>
+                <HashRouter>
+                    <Switch>
+                        <Route exact path="/" component={HomePage}/>
+                        <Route path="/login" component={LoginPage}/>
+                    </Switch>
+                </HashRouter>
+            </ConnectedRouter>
+        </Provider>
     );
 }
 
