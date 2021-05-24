fb0a27f229c750065e6156c8c44da04b2acf97c7
Reza Seidmoradi <Reza.Seidmoradi@outlook.com>
2021-05-24 11:46:00 +0430

    added two general constants for use in actions

---
+++ src/constants/User.js
@@ -0,0 +1,6 @@
+export const LOGIN = 'user/LOGIN';
+export const LOGIN_SUCCESS = 'user/LOGIN_SUCCESS';
+export const LOGIN_FAIL = 'user/LOGIN_FAIL';
+export const LOGOUT = 'user/LOGOUT';
+export const LOGOUT_SUCCESS = 'user/LOGOUT_SUCCESS';
+export const LOGOUT_FAIL = 'user/LOGOUT_FAIL';
